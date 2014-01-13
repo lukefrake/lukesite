ec062fdddd2f6e92bd639528a9471a8888ca99e1
Luke Frake <luke.frake@holidayextras.com>
2014-01-13 21:39:11 +0000

    Adding new grunt tasks and updating index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,42 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
-	  sass: {                              // Task
-	    dist: {                            // Target
-	      options: {                       // Target options
-	        style: 'expanded',
-	        cacheLocation: 'styles/.sass-cache'
-	      },
-	      files: {                         // Dictionary of files
-	        'styles/styles.css': 'styles/styles.scss',       // 'destination': 'source'
+		sass: {                              // Task
+			dist: {                            // Target
+				options: {                       // Target options
+					style: 'expanded',
+					cacheLocation: 'styles/.sass-cache'
+				},
+				files: {                         // Dictionary of files
+					'styles/styles.css': 'styles/styles.scss',       // 'destination': 'source'
+				}
+			}
+		},
+
+		watch: {
+			css: {
+				files: 'styles/*.scss',
+				tasks: ['sass', 'notify:sass'],
+			},
+			options: {
+				livereload: true,
+			},
+		},
+
+		notify: {
+	    sass: {
+	      options: {
+	        title: 'Task Complete',  // optional
+	        message: 'Look... SASS has converted to CSS', //required
 	      }
-	    }
-	  }
+	    },
+		},
+
 	});	
 	
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.registerTask('default', ['sass']);
+	grunt.loadNpmTasks('grunt-notify');
+	grunt.registerTask('default', ['watch']);
 
 };
