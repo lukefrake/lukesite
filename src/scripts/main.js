16a330986bdac2dec09fcfd8375f399b71e82578
Luke Frake <luke.frake@holidayextras.com>
2014-09-17 22:02:38 +0100

    Perf updates, style changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,35 @@
-$( ".burger" ).on( "click", function() {
-	if ( $( '.menu' ).hasClass( "active" ) ) {
-		$( '.menu' ).removeClass( 'active' );
-	} else {
-		$( '.menu' ).addClass( 'active' );
-	}
-});
+(function($,sr){
+
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null;
+          };
+
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
+
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  }
+  // smartresize 
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
+
+// Remove VH on the tile, because otherwise iOS has a fit
+if( navigator.userAgent.match( /(iPad|iPhone|iPod)/g ) ) {
+	// usage:
+	$( window ).smartresize( function(){
+	  $( ".tileSection" ).css( "min-height", "320px" );
+	} );
+}
